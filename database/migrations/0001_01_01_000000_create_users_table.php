fc7055bbfd55c10b002df9ba863b74b7a918636c
virtusx01 <muhammadnurfauzann88@gmail.com>
2025-06-27 06:45:44 +0700

    tahap 2

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('id_user')->unique(); 
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['owner','admin','customer'])->default('customer');
+            $table->boolean('status', [0, 1])->default(1);
+            $table->string('google_id')->nullable();
+            $table->string('google_avatar_url')->nullable();
+            $table->string('password'); 
+            $table->string('hp', 13)->default(""); 
+            $table->string('profile_photo_path')->nullable(); 
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
