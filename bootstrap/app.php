d98c3e736ef4b0bfa25e088cc9dc69d2925fb2ec
virtusx01 <muhammadnurfauzann88@gmail.com>
2025-06-27 16:36:19 +0700

    tahap 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'auth' => \App\Http\Middleware\Authenticate::class,
-            'role' => \App\Http\Middleware\CheckUserRole::class, // Tambahkan baris ini
-        ]);
+        
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
