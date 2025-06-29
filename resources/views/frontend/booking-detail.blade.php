3c85cc5e68c02621fbccad1e336a369a772c0ce0
virtusx01 <muhammadnurfauzann88@gmail.com>
2025-06-29 16:28:48 +0700

    tahap 4

--- resources/views/frontend/booking-detail.blade.php
+++ resources/views/frontend/booking-detail.blade.php
@@ -283,14 +283,6 @@
                         {{ $booking->status_label }}
                     </span>
                 </div>
-
-                
-                <div class="detail-item">
-                    <strong>Status:</strong>
-                    <span class="{{ $booking->status }}">
-                        {{ $booking->successfulPayment}}
-                    </span>
-                </div>
                 @if ($booking->status === 'pending' || $booking->status === 'challenge')
                     <div id="payment-polling-status" class="alert alert-info" style="margin-top: 15px; display: flex; align-items: center; gap: 10px;">
                         <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_V8m1{transform-origin:center;animation:spinner_zKoa 2s linear infinite}.spinner_V8m1 circle{stroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite}@keyframes spinner_zKoa{100%{transform:rotate(360deg)}}@keyframes spinner_YpZS{0%{stroke-dasharray:0 150;stroke-dashoffset:0}47.5%{stroke-dasharray:42 150;stroke-dashoffset:-16}95%,100%{stroke-dasharray:42 150;stroke-dashoffset:-59}}</style><g class="spinner_V8m1"><circle cx="12" cy="12" r="9.5" fill="none" stroke="#229954" stroke-width="3"></circle></g></svg>
