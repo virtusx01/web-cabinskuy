3e2d3bb24f51da499c1f58db9af8625d23bcd768
virtusx01 <muhammadnurfauzann88@gmail.com>
2025-06-29 16:43:16 +0700

    tahap 4

--- resources/views/frontend/booking-detail.blade.php
+++ resources/views/frontend/booking-detail.blade.php
@@ -287,7 +287,7 @@
                 <div class="detail-item">
                     <strong>Invoice:</strong>
                     <span class="">
-                        {{ $booking->successfulPayment }}
+                        {{ $payment->latestPayment }}
                     </span>
                 </div>
                 @if ($booking->status === 'pending' || $booking->status === 'challenge')
